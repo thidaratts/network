d54fbecdd6ee9967cb0f7c710d7c3d97f97c2764
thida <thidarat.jtrchot@gmail.com>
2018-05-15 22:14:32 +0700

    1st

---
+++ ClientTFTP.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+
+//TFTP Client
+
+
+
+void enErrorMessage(char ep[4], short int errMessage){
+        short int tmp = htons(5);
+        memcpy(ep, &tmp, 2);
+        tmp = htons(errMessage);
+        memcpy(ep+2, &errMessage, 2);
+}
+
+void decodeErrorMessage(int r, char code[], char errMessage[128]){
+        short int tmp, en ;
+        memcpy(&tmp, code+2, 2);
+        en = ntohs(tmp);
+        switch(en){
+                case 0 : strcpy(errMessage, "Not defined");             break;
+                case 1 : strcpy(errMessage, "File not found");          break;
+                case 2 : strcpy(errMessage, "Access violation");        break;
+                case 3 : strcpy(errMessage, "Disk full");               break;
+                case 4 : strcpy(errMessage, "Illegal TFTP operation");  break;
+                case 5 : strcpy(errMessage, "Unknow port");             break;
+                case 6 : strcpy(errMessage, "File already exists");     break;
+                case 7 : strcpy(errMessage, "No such user");            break;
+        }
+}
+
+
+void deDP(char dp[516], int r, short int *opcode, short int *blockno, char data[512]){
+	short int tmp;
+	memcpy(&tmp, dp, 2);
+	*opcode = ntohs(tmp);
+	memcpy(&tmp, dp+2, 2);
+	*blockno = ntohs(tmp);
+	memcpy(data, dp+4, r-4); 
+}
+
+int checkOp(char blockIN[]){
+        short int tmp;
+        memcpy(&tmp, blockIN, 2);
+        return ntohs(tmp);
+}
+
+
+void enACK(char ack[4], short blockno){	
+	short int tmp;
+	tmp = htons(4);
+	memcpy(ack, &tmp, 2);
+	tmp = htons(blockno);
+	memcpy(ack+2, &tmp, 2);
+}
+
+int enRW(char bd[139], short int opcode, char filename[128], char mode[9]){
+
+	short int tmp = htons(opcode);
+	memcpy(bd, &tmp, 2);
+	memcpy(bd+2, filename, strlen(filename)+1);
+	memcpy(bd+2+strlen(filename)+1, mode, strlen(mode)+1);
+
+	printf("bd = %d:%s:%s\n", opcode, filename, mode);
+
+	return 2+strlen(filename)+1+strlen(mode)+1;
+}
+
+int main(){
+
+	int client_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	
+	struct sockaddr_in server_address;
+	bzero((char*)&server_address, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(9002);
+	server_address.sin_addr.s_addr = INADDR_ANY;
+
+	char bd[139];
+        int len = enRW(bd, 1, "ReadFile.txt", "octet");
+
+	sendto(client_socket, bd, len, 0, (struct sockaddr*)&server_address, sizeof(server_address));
+	printf("send read req\n");
+
+	char dp[516], data[512];
+	short int opcode, blockno;
+
+	//create timeout
+	
+
+	int r = recvfrom(client_socket, dp, 516, 0, (struct sockaddr*)0, (int*)0);
+	
+	//calcelTimeout and checkTimeout
+        
+	printf("r = %d\n",r);
+	if(checkOp(dp)==5){
+		//error
+		char errMessage[128];
+                decodeErrorMessage(r, dp, errMessage);
+                printf("!!! got err message : %s!!!\n", errMessage);
+		return 1;
+	}else if(checkOp(dp)==3){
+		deDP(dp, r, &opcode, &blockno, data);
+		printf("got dp block = %d\n", blockno);
+		printf("========data==============\n%s\n===============\n", data);
+		int fd = open("WritFile.txt", O_WRONLY);
+		short int blockno2 = 1;
+		short int blockno3;
+		short int opcode2;
+		if(fd<0){
+			//create file
+			printf("You have to create file name myFile.txt 1st\n");
+		}
+		while(1){
+			//printf("r = %d\n", r);
+			write(fd, data, r-4);
+			if(r<512){
+				printf("\n\nDONE\n\n");
+				break;
+			}
+			//memset(data, 0, sizeof(data));
+			char ack[4];
+			enACK(ack, blockno2);
+			sendto(client_socket, ack, 4, 0, (struct sockaddr*)&server_address, sizeof(server_address));
+			printf("send ack block %d\n", blockno2);
+	
+
+			//create timeout
+			
+			r = recvfrom(client_socket, dp, 516, 0, (struct sockaddr*)0, (int*)0);
+		
+			//cancelTimeout and checkTimeout
+        		
+
+
+			printf("r = %d\n", r);
+			if(checkOp(dp)==5){
+				//error
+				char errMessage[128];
+                		decodeErrorMessage(r, dp, errMessage);
+                		printf("!!! got err message : %s!!!\n", errMessage);
+				return 1;
+			}else if(checkOp(dp)==3){
+				deDP(dp, r, &opcode2, &blockno3, data);
+				printf("got dp block = %d\n", blockno3);
+				printf("========data==============\n%s\n===============\n", data);
+				blockno2++;
+			}
+		}
+	}
+
+	//printf("@@@ test @@@\n\n");
+	close(client_socket);
+
+	return 0;
+}
---
+++ ServerTFTP.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+
+//TFTP Server
+
+void ErrorMessage(char ep[4], short int errMessage){
+	short int tmp = htons(5);
+	memcpy(ep, &tmp, 2);
+	tmp = htons(errMessage);
+	memcpy(ep+2, &errMessage, 2);
+}
+
+void DecodeErrorMessage(int r, char code[], char errMessage[128]){
+	short int tmp, en ;
+	memcpy(&tmp, code+2, 2);
+	en = ntohs(tmp); 
+	switch(en){
+		case 0 : strcpy(errMessage, "Not defined"); 		break;
+		case 1 : strcpy(errMessage, "File not found"); 		break;
+		case 2 : strcpy(errMessage, "Access violation"); 	break;
+		case 3 : strcpy(errMessage, "Disk full"); 		break;
+		case 4 : strcpy(errMessage, "Illegal TFTP operation"); 	break;
+		case 5 : strcpy(errMessage, "Unknow port"); 		break;
+		case 6 : strcpy(errMessage, "File already exists"); 	break;
+		case 7 : strcpy(errMessage, "No such user"); 		break;
+	}
+}
+
+void BlockRW(int r, char code[139],short int *opcode, char filename[128], char mode[9]){
+
+	short int tmp;
+	char temp[128];
+	memcpy(&tmp, code, 2);
+	memcpy(temp, code+2, strlen(code+2)+1);
+	memcpy(mode, code+2+strlen(temp)+1, r-strlen(temp)+3);
+	memcpy(filename, temp, strlen(temp)+1);
+
+	*opcode = ntohs(tmp);
+}
+
+int Op(char blockIN[]){
+	short int tmp;
+	memcpy(&tmp, blockIN, 2);
+	return ntohs(tmp);
+}
+
+void enDP(char dp[516], short int no, char data[512]){
+	short int tmp = htons(3);
+	short int tmno = htons(no);
+	memcpy(dp, &tmp, 2);
+	memcpy(dp+2, &tmno, 2);
+	memcpy(dp+4, data, strlen(data)+1);
+}
+
+void deACK(char code[4], short int *opcode, short int *blockno){
+	short int tmp;
+
+	memcpy(&tmp, code, 2);
+	*opcode = ntohs(tmp);
+	memcpy(&tmp, code+2, 2);
+	*blockno = ntohs(tmp);
+}
+
+int main(){
+
+	int server_socket = server_socket = socket(AF_INET, SOCK_DGRAM, 0);
+
+	struct sockaddr_in server_address, client_address;
+	bzero((char*)&server_address, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(9002);
+	server_address.sin_addr.s_addr = INADDR_ANY;
+
+	bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
+
+	socklen_t addr_size = sizeof(client_address);
+	char rd[139];
+
+	//create timeout
+	
+	int r = recvfrom(server_socket, rd, 139, 0, (struct sockaddr*)&client_address, &addr_size);
+
+	//cancelTimeout and checkTimeout
+   
+
+
+	char filename[128], mode[9];
+	short int opcode;
+
+	if(Op(rd)==1 || Op(rd)==2){
+		BlockRW(r, rd, &opcode, filename, mode);
+	}else if(Op(rd)==5){
+		//error
+		char errMessage[128];
+		DecodeErrorMessage(r, rd, errMessage);
+		printf("!!! got err message : %s!!!\n", errMessage);
+		return 1;
+	}
+	printf("got RW opcode = %d\n", opcode);
+
+	if(opcode == 1){
+		int fd = open(filename, O_RDONLY);
+		if(fd<0){
+			//send err file not found
+			char ep[4];
+                	ErrorMessage(ep, 1);
+			sendto(server_socket, ep, 4, 0, (struct sockaddr*)&client_address, addr_size);
+		}
+		char buf[512], dp[516];
+		short int blockno = 1;
+		int byteread;
+		while((byteread = read(fd, buf, sizeof(buf)))!=0){
+		
+			enDP(dp, blockno, buf);
+			sendto(server_socket, dp, byteread+4, 0, (struct sockaddr*)&client_address, addr_size);
+			
+			if(byteread<512){
+				printf("\n\nDONE\n\n");
+				break;
+			}
+
+			printf("send dp block %d\n", blockno);
+			printf("=========data===========\n%s\n============\n", buf);
+			memset(buf, 0, sizeof(buf));
+
+			char ack[4];
+			short int opcode2;
+			short int blockno2;
+
+			//create timeout
+			
+			recvfrom(server_socket, ack, 4, 0, (struct sockaddr*)&client_address, &addr_size);
+			
+			//cancelTimeout and checkTimeout
+        		
+        		
+
+
+			if(Op(ack)==5){
+				//error
+				char errMessage[128];
+                		DecodeErrorMessage(r, ack, errMessage);
+ 		                printf("!!! got err message : %s!!!\n", errMessage);
+				return 1;
+			}else if(Op(ack)==4){
+				deACK(ack, &opcode2, &blockno2);
+				printf("got ack block %d\n", blockno2);
+				blockno++;
+			}
+		}
+	
+	}
+
+
+	return 0;
+}
